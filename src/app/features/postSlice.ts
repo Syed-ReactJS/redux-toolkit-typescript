0aca974e52768da5b5b72b05f4f27b2354dc654f
hasnain2025 <mehadi.2025@gmail.com>
2022-09-24 16:00:46 +0530

    async thunk added or api call using redux toolkit

--- src/app/features/postSlice.ts
+++ src/app/features/postSlice.ts
@@ -1,36 +1,94 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface PostState {
   value: {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+    post: string[];
+    newPost: {};
   };
+  loading: boolean;
+  msg: string;
 }
 const initialState: PostState = {
   value: {
-    userId: 0,
-    id: 0,
-    title: "",
-    body: "",
+    post: [],
+    newPost: {},
   },
+  loading: false,
+  msg: "",
 };
 
+//async thunk
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET",
+    redirect: "follow",
+  });
+  const data = await response.json();
+  console.log("res", data);
+
+  return data;
+});
+
+export const createPost = createAsyncThunk(
+  "posts/createPost",
+  async (payload: any) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      method: "POST",
+      body: payload,
+    });
+    const data = await response.json();
+    console.log("res", data);
+
+    return data;
+  }
+);
+
 export const counterSlice = createSlice({
-  name: "counter",
+  name: "posts",
   initialState,
   reducers: {
-    createPost: (state, action) => {
-      console.log("state", state);
-      console.log("action", action);
-      // state.value += 1;
-    },
+    // omit existing reducers here
+  },
+  extraReducers(builder) {
+    builder
+      //for getting  posts
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.loading = true;
+        state.msg = "Pending";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.msg = "Success";
+        state.value.post = action.payload;
+        //action.payload?.data?.title ??
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.msg = "Failed";
+      })
+
+      //for creating  posts
+      .addCase(createPost.pending, (state, action) => {
+        state.loading = true;
+        state.msg = "Pending";
+      })
+      .addCase(createPost.fulfilled, (state, action) => {
+        state.loading = false;
+        state.msg = "Success";
+        state.value.newPost = action.payload;
+      })
+      .addCase(createPost.rejected, (state, action) => {
+        state.loading = false;
+        state.msg = "Failed";
+      });
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { createPost } = counterSlice.actions;
+// export const { fetchPosts } = counterSlice.actions;
 
 export default counterSlice.reducer;
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,14 +1,35 @@
-import { useSelector } from "react-redux";
-// import { createPost } from "../../app/features/postSlice";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { createPost, fetchPosts } from "../../app/features/postSlice";
 import { RootState } from "../../app/store";
 
 const Post = () => {
-  const store = useSelector((state: RootState) => state);
-  console.log("store", store);
+  const dispatch = useDispatch<any>();
+  const store = useSelector((state: RootState) => state?.post?.loading);
+  useEffect(() => {
+    console.log("store", store);
+  }, [store]);
+
   return (
     <div>
       <h1>Post</h1>
-      <button>get Data & set in store </button>
+      <button onClick={() => dispatch(fetchPosts())}>
+        get Data & set in store{" "}
+      </button>
+      <button
+        onClick={() =>
+          dispatch(
+            createPost(
+              JSON.stringify({
+                title: "foo",
+                body: "bar",
+                userId: 1,
+              })
+            )
+          )
+        }>
+        Create new Post
+      </button>
     </div>
   );
 };
